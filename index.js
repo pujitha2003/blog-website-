907e6f5d21e856e80a59415b2806e9d1dbb11650
pujitha2003 <129185856+pujitha2003@users.noreply.github.com>
2023-11-30 19:40:01 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const path = require('path');
+const app = express();
+const mongoDb = require('./conn');
+const router = require('./routes/blogRoute');
+
+dotenv.config();
+const port = process.env.PORT || 8080;
+const dburl = process.env.DB_URI;
+
+const staticPath = path.join(__dirname, 'public');
+
+// Database connect
+mongoDb(dburl);
+
+// Middlewares
+app.use(express.json());
+app.use('/api', router);
+app.use(express.static(staticPath));
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(staticPath, 'index.html'));
+});
+
+app.get('/create', (req, res) => {
+  res.sendFile(path.join(staticPath, 'create.html'));
+});
+
+app.get('/blogs/:id', (req, res) => {
+  res.sendFile(path.join(staticPath, 'readblog.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
